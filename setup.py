68e63f19eee596033515fd12bff6fc5878366499
yjchoe <yjchoe@cmu.edu>
2022-02-17 11:51:09 -0500

    Bump to v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='comparecast',
-    version='0.1.0',
+    version='0.1.1',
     packages=['comparecast', 'comparecast.data_utils'],
     python_requires='>=3.7',
     install_requires=[
